66d681dae0725ab5ec449aa8f7ec57f2d9e93ede
DoQDuong <110405690+DoQDuong@users.noreply.github.com>
2022-11-23 16:06:33 +0700

    Create exam.sql

---
+++ sql/exam.sql
@@ -0,0 +1,30 @@
+﻿create table Customer(
+CustomerId int primary key,
+Name nvarchar(50),
+City nvarchar(50),
+Country nvarchar(50),
+Phone nvarchar(50),
+Email nvarchar(50)
+);
+create table CustomerAccount(
+AccountNumber char(9) primary key,
+CustomerId int foreign key references Customer(Customerid),
+Balance money not null check (Balance>=0),
+MinAccount money												
+);
+create table CustomerTransaction(
+TransactionId int primary key,
+AccountNumber char(9),
+TransactionDate smalldatetime check(transactiondate <=getdate()) default getdate(),
+Amount money,
+DepositorWithdraw bit
+);
+
+insert into Customer(CustomerId,Name,City,Country,Phone,Email)
+values ('123456', N'Nguyễn Văn An',N'Hà Nội',N'Việt Nam','0123456789',N'nguyenvanan@gmail.com');
+
+select * from Customer;
+
+insert into CustomerAccount(AccountNumber,CustomerId,Balance,MinAccount)
+values ('123456','9','88886666','0');
+select * from CustomerAccount;
